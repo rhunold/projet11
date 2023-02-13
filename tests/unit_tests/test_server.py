339b6bf41d1a93ad0c90a59387c00517a1dc11f3
Hunold <raphael.hunold@gmail.com>
2023-02-13 16:00:35 +0100

    adding the test if place required > 12 for a competition

--- tests/unit_tests/test_server.py
+++ tests/unit_tests/test_server.py
@@ -17,12 +17,11 @@ def test_login_invalid_email(client):
     assert response.status_code == 200
 
 
-# tests clubs should not be able to use more than their points allowed
+# Clubs should not be able to use more than their points allowed
 def test_purchasePlaces_not_enough_points(client, clubs_tests, competitions_tests):
-    
-    club_test = clubs_tests[1]
-    competition_test = competitions_tests[1]
-    places = 12
+    club_test = clubs_tests[0]
+    competition_test = competitions_tests[0]
+    places = 5
     response = client.post('/purchasePlaces', data={"club": club_test["name"],
                                                     "competition": competition_test["name"],
                                                     "places": places})
@@ -31,4 +30,18 @@ def test_purchasePlaces_not_enough_points(client, clubs_tests, competitions_test
 
     assert response.status_code == 200
     assert "You don\'t have enought points." in data
+    
+    
+# Clubs shouldn't be able to book more than 12 places per competition
+def test_purchasePlaces_not_more_12points_per_competition(client, clubs_tests, competitions_tests):
+    club_test = clubs_tests[1]
+    competition_test = competitions_tests[1]
+    places = 13
+    response = client.post('/purchasePlaces', data={"club": club_test["name"],
+                                                    "competition": competition_test["name"],
+                                                    "places": places})
+
+    data = response.data.decode()
+    assert response.status_code == 200
+    assert "You can\'t reserve more than 12 places for a competition." in data
 
